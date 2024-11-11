aab821869777618a15a7148f4ce1aea92d6377e0
牛锴鹏 <103653394+MeiTaylor@users.noreply.github.com>
2024-11-12 01:10:52 +0800

    添加新的DTO字段

---
+++ Main Code/Backend/src/main/java/com/ylw/backend/dto/WebSendResumeId.java
@@ -0,0 +1,12 @@
+package com.ylw.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WebSendResumeId {
+    int resumeId;
+}
